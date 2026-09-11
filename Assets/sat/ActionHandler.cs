54b28fefe09da44dbe8c8249b1a9aa1fdc7500bb
kii borde <satellite6@tutanota.com>
2026-09-11 20:09:54 +0000

    Implemented Interface

--- Assets/sat/ActionHandler.cs
+++ Assets/sat/ActionHandler.cs
@@ -1,12 +1,22 @@
-using System.IO.Compression;
 using UnityEngine;
 public class  ActionHandler : MonoBehaviour
 {
     Gesture playerAction;
-    Gesture broAction;
+    Gesture enemyAction;
+    
+    // Interface Begin
+    public void OnGestureButtonPress(int action) {
+        playerAction = (Gesture)action;
+    }
+    public void EndTurn()
+    {
+        enemyAction = EnemyDecide();
+        InflictOutcome();
+    }
 
+    // Interface End
 
-    public enum Gesture
+    enum Gesture
     {
         Rock,
         Paper,
@@ -14,7 +24,6 @@ public class  ActionHandler : MonoBehaviour
         MiddleFinger,
         Gun,
     }
-
     private void InflictOutcome() {
         if (IsPlayerAdvantageous()) { print("bro took damage"); } else { print("player took damage"); }
     }
@@ -23,27 +32,27 @@ public class  ActionHandler : MonoBehaviour
         // quick event calll
         // calls quick event
         // actually it calls the quick event if the gfesturem  is kquiuck event calling wworthy
-        if (broAction == Gesture.Gun) { print("trigger quick action"); }
+        if (enemyAction == Gesture.Gun) { print("trigger quick action ecentr tune, respond to gun in time with middle finger"); }
         else
         {
             switch (playerAction)
             {
                 case Gesture.Rock:
                     // rock loses to paper you're supposed to know that smh
-                    if (broAction == Gesture.Paper) { return false; } else { return true; }
+                    if (enemyAction == Gesture.Paper) { return false; } else { return true; }
                 case Gesture.Paper:
                     // Paper got cut ouchie ow ow
-                    if (broAction == Gesture.Scissors) { return false; } else { return true; }
+                    if (enemyAction == Gesture.Scissors) { return false; } else { return true; }
                 case Gesture.Scissors:
                     // euggh
-                    if (broAction == Gesture.Rock) { return false; } else { return true; }
+                    if (enemyAction == Gesture.Rock) { return false; } else { return true; }
                 case Gesture.MiddleFinger:
                     // bro is sad and hates you
                     Application.Quit();
                     break;
                 case Gesture.Gun:
                     // bro is- SHUT UP SHU*T UP I WIN
-                    Debug.Log("your did it");
+                    print("your did it");
                     break;
                 default:
                     Application.Quit();
@@ -51,5 +60,12 @@ public class  ActionHandler : MonoBehaviour
             }
         }
         return false;
+    }
+
+    private Gesture EnemyDecide() {
+        // Fuckass function
+        int decision = Random.Range(0, Gesture.GetNames(typeof(Gesture)).Length); 
+        Gesture decisionGesture = (Gesture)decision;
+        return decisionGesture;
     }
 }
