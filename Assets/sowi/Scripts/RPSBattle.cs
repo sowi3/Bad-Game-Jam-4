488eb1aaa2fcdfc53d0d235960133dbbff93f15f
sowi3 <159765631+sowi3@users.noreply.github.com>
2026-09-11 22:10:02 +0200

    hi

--- Assets/sowi/Scripts/RPSBattle.cs
+++ Assets/sowi/Scripts/RPSBattle.cs
@@ -8,7 +8,7 @@ public class RPSBattle : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(shit);
+        Instantiate(shit, transform);
     }
 
     // Update is called once per frame
